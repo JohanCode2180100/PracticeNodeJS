d6fd1103ee302eb6e9db04b7bfc66c4dcc1ee1ca
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-07-26 20:33:31 +0200

    add pico css and app.post for movies

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const port = 3000;
 const app = express();
+const bodyParser = require("body-parser");
 
 //permet de servir les fichiers static
 app.use("/style", express.static("style"));
-
+//on choisi le middleware du bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
 //utilisation du moteur de template
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 //attention a l ordre des routes, get.id est plus specialise et viendra avant get/add
 
+/*---------------------------------------------------------
+------------------------ POST -----------------------------
+-----------------------------------------------------------*/
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.post("/movies", urlencodedParser, (req, res) => {
+  //ne pas oublier body-parser
+  console.log(req.body);
+  res.sendStatus(201);
+});
+
+/*---------------------------------------------------------
+------------------------ GET -----------------------------
+-----------------------------------------------------------*/
 
 app.get("/movies", (req, res) => {
   const title = "films francais";
@@ -23,6 +38,9 @@ app.get("/movies", (req, res) => {
       title: "la ligne verte",
     },
     { title: "GhostWrtitter" },
+    {
+      title: "la 7 eme confirmmpagnie au clair de lune",
+    },
   ];
   res.render("movies", { movies: frenchMovies, title: title });
 });
